c0cc036a1df4405dfb97b4f13016adc04539d94f
Carolina <cpprates18@gmail.com>
2019-02-20 22:46:43 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# on-it-mobile
+
+# Task management/ToDo list with motivational messages to keep you reaching your goals!
+
+# Digital Learners to Leaders Program at Georgia State University
---
+++ src/app/items/item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "Item",
+	moduleId: module.id,
+	templateUrl: "./item.component.html"
+
+})
+export class ItemComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+}
